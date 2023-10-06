6e762da3555342f1806ac89b44eaa5dd80fc6895
Evin06 <96207677+Evin06@users.noreply.github.com>
2023-10-06 10:33:43 +0200

    Add files via upload

---
+++ C#_SpaceInvader/Display/Playground.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+
+namespace Display
+{
+    public class Playground
+    {
+        public static void Init()
+        {
+            Console.CursorVisible = false;
+        }
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+       public const int SHEIGHT = 50;
+       public const int SWIDTH = 150;
+    }
+}
