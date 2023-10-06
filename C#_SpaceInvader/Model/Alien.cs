650754057da5731c94b10216e2914dc92ecbecf9
Evin06 <96207677+Evin06@users.noreply.github.com>
2023-10-06 10:34:26 +0200

    Add files via upload

---
+++ C#_SpaceInvader/Model/Alien.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Alien
+    {
+        private string[] view =
+      {
+            @"   ▄ ▀▄  ▄▀ ▄    ",
+            @"   █▄██████▄█    ",
+            @"   ███▄██▄███    ",
+            @"   ▀████████▀    ",
+            @"    ▄▀    ▀▄     ",
+        };
+
+
+        public int x;
+        public int y;
+        public bool limit = true;
+        public void Draw()
+        {
+
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.WriteLine(view[i]);
+            }
+        }
+        public void Move()
+        {
+
+            if (limit == true)
+            {
+                x++;
+                if (x == Console.WindowWidth - 15)
+                {
+                    y++;
+                    limit = false;
+                }
+
+            }
+            if (limit == false)
+            {
+                x--;
+                if (x == 0 )
+                {
+                    y++;
+                    limit = true;
+                }
+
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ C#_SpaceInvader/Model/Canon.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+
+    public class Canon
+    {
+        private string[] view =
+      {
+            @"       |       ",
+            @"      / \      ",
+            @"     / _ \     ",
+            @"  | | ( ) | |  ",
+            @"  |-|     |-|  ",
+            @"    |  _  |    ",
+            @"|__/  | |  \__|  ",
+            @"  /   |_|   \  ",
+            @" |___________| ",
+            @"   |_|_|_|_|  ",
+            @"    WWWWWWW ",
+        };
+
+
+        public int x = 65;
+        public int y = 35;
+        public bool move = true;
+        public void Draw()
+        {
+
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.WriteLine(view[i]);
+            }
+        }
+        public void Move()
+        {
+
+        }
+        public void MoveRight()
+        {
+            if (x == (Console.WindowWidth - 15))
+            {
+                x = Console.WindowWidth - 15;
+            }
+            else
+            {
+                x++;
+            }
+
+        }
+        public void MoveLeft()
+        {
+            if (x == 0)
+            {
+                x = 0;
+            }
+            else
+            {
+                x--;
+            }
+        }
+    }
+}
