bc7befa798f00f685e8eab93eacf4d3b4732586c
Evin06 <96207677+Evin06@users.noreply.github.com>
2023-11-03 11:16:59 +0100

    modification rapport

---
+++ C#_SpaceInvader/ModelTests/CanonTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class CanonTests
+    {
+        [TestMethod()]
+        public void MoveLeftTest()
+        {
+            Canon canon = new Canon();
+            canon.x = 3; 
+
+            // Act
+            canon.MoveLeft();
+
+            // Assert
+            Assert.AreEqual(1, canon.x);
+        }
+
+        [TestMethod()]
+        public void MoveRightTest()
+        {
+            // Arrange
+            Canon canon = new Canon();
+            canon.x = Console.WindowWidth - 17; // Setting an initial value less than Console.WindowWidth - 15
+
+            // Act
+            canon.MoveRight();
+
+            // Assert
+            Assert.AreEqual(Console.WindowWidth - 15, canon.x);
+        }
+    }
+}
